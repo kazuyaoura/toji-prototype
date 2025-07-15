e7e4a35d34d003c8cc03440ca0516b87d4f252a9
kazuyaoura <otu_nishiguchi13@yahoo.co.jp>
2025-07-15 17:21:03 +0900

    Create dialogueData.ts

---
+++ app/prologue/dialogueData.ts
@@ -0,0 +1,18 @@
+// app/prologue/dialogueData.ts
+
+const dialogue = [
+  {
+    speaker: '吉左衛門',
+    text: 'よう来てくれたな。',
+  },
+  {
+    speaker: '吉左衛門',
+    text: '杜氏としての腕、見せてもらおうか。',
+  },
+  {
+    speaker: '吉左衛門',
+    text: 'まずは、この蔵の様子を見て回るがいい。',
+  },
+];
+
+export default dialogue;
