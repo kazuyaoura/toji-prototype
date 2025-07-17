7113b6d4a033a18439cfb918351c26ec2eeff713
kazuyaoura <otu_nishiguchi13@yahoo.co.jp>
2025-07-17 21:33:34 +0900

    Update TitleScreen.tsx

--- app/components/TitleScreen.tsx
+++ app/components/TitleScreen.tsx
@@ -1,13 +1,17 @@
-// app/components/TitleScreen.tsx
-import React from 'react';
-
-const TitleScreen = () => {
-  return (
-    <div className="flex flex-col items-center justify-center h-screen bg-black text-white">
-      <h1 className="text-4xl font-bold mb-4">今日から杜氏！</h1>
-      <p className="text-lg">本蔵へようこそ。</p>
-    </div>
-  );
+type Props = {
+  onStart: () => void;
 };
 
-export default TitleScreen;
+export default function TitleScreen({ onStart }: Props) {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold mb-4">今日から杜氏！</h1>
+      <button
+        onClick={onStart}
+        className="px-4 py-2 bg-blue-500 text-white rounded"
+      >
+        はじめる
+      </button>
+    </div>
+  );
+}
