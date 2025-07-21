5745920f536660e962ec2905c1229ea0396758e6
kazuyaoura <otu_nishiguchi13@yahoo.co.jp>
2025-07-21 18:16:36 +0900

    Create button.tsx

---
+++ components/ui/button.tsx
@@ -0,0 +1,20 @@
+// components/ui/button.tsx
+'use client';
+
+import * as React from 'react';
+
+type ButtonProps = {
+  children: React.ReactNode;
+  onClick?: () => void;
+};
+
+export function Button({ children, onClick }: ButtonProps) {
+  return (
+    <button
+      onClick={onClick}
+      className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
+    >
+      {children}
+    </button>
+  );
+}
