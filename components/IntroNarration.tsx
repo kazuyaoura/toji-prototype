8e62ca5b9f1ba25f4e88d00c3ff38354ac2937cc
kazuyaoura <otu_nishiguchi13@yahoo.co.jp>
2025-07-23 08:59:08 +0900

    Update IntroNarration.tsx

--- components/IntroNarration.tsx
+++ components/IntroNarration.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 type Props = {
   text: string;
   onNext: () => void;
@@ -6,11 +8,56 @@ type Props = {
 export default function IntroNarration({ text, onNext }: Props) {
   return (
     <div
-      className="flex flex-col items-center justify-center min-h-screen p-6 bg-white text-xl text-center space-y-6"
       onClick={onNext}
+      style={{
+        width: "100vw",
+        height: "100vh",
+        position: "relative",
+        overflow: "hidden",
+      }}
     >
-      <p>{text}</p>
-      <p className="text-sm text-gray-400">(タップして進む)</p>
+      {/* 背景画像 */}
+      <img
+        src="/images/bg_narration_kichizaemon_intro.png"
+        alt="ナレーション背景"
+        style={{
+          width: "100%",
+          height: "100%",
+          objectFit: "cover",
+          position: "absolute",
+        }}
+      />
+
+      {/* コメントウィンドウ */}
+      <img
+        src="/images/ui_comment_window_base.png"
+        alt="コメントボックス"
+        style={{
+          position: "absolute",
+          bottom: 0,
+          left: 0,
+          width: "100%",
+          height: "25vh",
+          objectFit: "cover",
+        }}
+      />
+
+      {/* テキスト表示 */}
+      <div
+        style={{
+          position: "absolute",
+          bottom: "6vh",
+          left: "5vw",
+          right: "5vw",
+          color: "#222",
+          fontSize: "1.2rem",
+          lineHeight: "1.8",
+          fontWeight: "bold",
+          textShadow: "1px 1px 2px rgba(255,255,255,0.8)",
+        }}
+      >
+        {text}
+      </div>
     </div>
   );
 }
