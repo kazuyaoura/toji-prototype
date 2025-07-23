dbcb8e5f6fec47c9e3ef81bb19f3d1cbd0a211be
kazuyaoura <otu_nishiguchi13@yahoo.co.jp>
2025-07-23 12:08:48 +0900

    Update MoneyDisplay.tsx

--- components/MoneyDisplay.tsx
+++ components/MoneyDisplay.tsx
@@ -8,27 +8,16 @@ export default function MoneyDisplay() {
   const { money } = useContext(MoneyContext);
 
   return (
-    <div style={{ position: 'absolute', top: 10, right: 10, width: 120 }}>
-      <div style={{ position: 'relative', width: '100%' }}>
+    <div className="absolute top-2 right-2 w-[120px] z-50">
+      <div className="relative w-full">
         <Image
           src="/ui_money_display_board.png"
           alt="所持金"
           width={120}
           height={100}
-          style={{ width: '100%', height: 'auto' }}
+          className="w-full h-auto"
         />
-        <span
-          style={{
-            position: 'absolute',
-            top: '58%',
-            left: '50%',
-            transform: 'translate(-50%, -50%)',
-            fontWeight: 'bold',
-            fontSize: '18px',
-            color: '#000',
-            textShadow: '1px 1px 1px #fff',
-          }}
-        >
+        <span className="absolute top-[58%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 font-bold text-[18px] text-black text-outline">
           ¥{money.toLocaleString()}
         </span>
       </div>
