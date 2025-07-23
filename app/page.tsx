63e8948410e36a2cf63c00b37dd22c7ddece52c0
kazuyaoura <otu_nishiguchi13@yahoo.co.jp>
2025-07-23 10:25:56 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,15 @@
 
 import GameContainer from '@/components/GameContainer';
 import MoneyDisplay from "@/components/MoneyDisplay";
-import { useContext } from "react";
-import { MoneyContext } from "@/contexts/MoneyContext";
+import { MoneyProvider } from "@/contexts/MoneyContext";
 
 export default function Home() {
-  return <GameContainer />;
+  return (
+    <MoneyProvider>
+      <>
+        <MoneyDisplay />
+        <GameContainer />
+      </>
+    </MoneyProvider>
+  );
 }
