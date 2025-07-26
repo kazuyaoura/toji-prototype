4130de472d2d32a204350c0d2e16f1d5aefcac59
kazuyaoura <otu_nishiguchi13@yahoo.co.jp>
2025-07-26 21:24:37 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,15 @@
 'use client';
 
-import { useState } from 'react';
-import TitleScreen from '@/components/TitleScreen';
 import GameContainer from '@/components/GameContainer';
 import MoneyDisplay from '@/components/MoneyDisplay';
 import { MoneyProvider } from '@/contexts/MoneyContext';
 
 export default function Home() {
-  const [step, setStep] = useState(0);
-
   return (
     <MoneyProvider>
       <>
         <MoneyDisplay />
-        {step === 0 ? (
-          <TitleScreen onStart={() => setStep(1)} />
-        ) : (
-          <GameContainer />
-        )}
+        <GameContainer />
       </>
     </MoneyProvider>
   );
