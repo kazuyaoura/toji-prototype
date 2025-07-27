887b119cd8ca72258d667c9b7566d2efd91f664f
kazuyaoura <otu_nishiguchi13@yahoo.co.jp>
2025-07-27 14:05:43 +0900

    Update NoticeScreen.tsx

--- components/NoticeScreen.tsx
+++ components/NoticeScreen.tsx
@@ -1,26 +1,33 @@
 'use client';
 
-import React from 'react';
 import Image from 'next/image';
-import { Button } from '@/components/ui/button';
 
-type NoticeScreenProps = {
+type Props = {
   onNext: () => void;
 };
 
-export default function NoticeScreen({ onNext }: NoticeScreenProps) {
+export default function NoticeScreen({ onNext }: Props) {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-black">
+    <div className="relative w-full h-screen bg-white overflow-hidden">
+      {/* 背景画像 */}
       <Image
-        src="/images/ui_notice_game_disclaimer.png"
-        alt="注意事項"
-        width={800}
-        height={600}
-        className="rounded-lg shadow-lg"
+        src="/images/notice_screen_vertical.png"
+        alt="ご注意"
+        width={1080}
+        height={1920}
+        className="absolute top-0 left-0 w-full h-full object-contain pointer-events-none"
+        priority
       />
-      <Button onClick={onNext} className="mt-6">
-        つぎへ
-      </Button>
+
+      {/* ▶ 次へボタン（画面下中央） */}
+      <div className="absolute bottom-[8%] left-1/2 z-10 transform -translate-x-1/2">
+        <button
+          onClick={onNext}
+          className="px-7 py-3 text-xl font-semibold bg-white rounded-xl shadow-md hover:bg-gray-100 active:scale-95 transition-all duration-150"
+        >
+          ▶ 次へ
+        </button>
+      </div>
     </div>
   );
 }
