96bf10119dfae6ea8a8c5f901f19c351dbe19c86
kazuyaoura <otu_nishiguchi13@yahoo.co.jp>
2025-08-08 12:32:11 +0900

    Update Instructions.tsx

--- components/Instructions.tsx
+++ components/Instructions.tsx
@@ -6,15 +6,22 @@ type Props = {
 
 export default function Instructions({ onNext }: Props) {
   return (
-    <div
-      onClick={onNext}
-      className="w-screen h-screen overflow-hidden m-0 p-0 relative cursor-pointer"
-    >
+    <div className="w-screen h-screen overflow-hidden m-0 p-0 relative bg-black">
+      {/* 背景画像 */}
       <img
         src="/images/ui_notice_game_disclaimer.png"
         alt="ご注意"
-        className="w-full h-full object-cover"
+        className="w-full h-full object-contain"
+        draggable={false}
       />
+
+      {/* 次へボタン */}
+      <button
+        onClick={onNext}
+        className="absolute bottom-8 left-1/2 transform -translate-x-1/2 px-6 py-2 bg-blue-600 text-white rounded shadow-lg hover:bg-blue-700 active:scale-95 transition"
+      >
+        ▶ 次へ
+      </button>
     </div>
   );
 }
