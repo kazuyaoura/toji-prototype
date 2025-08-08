9cb7b242fdca4fba8243bc02d62444a0168d57dc
kazuyaoura <otu_nishiguchi13@yahoo.co.jp>
2025-08-08 12:02:27 +0900

    Update TitleScreen.tsx

--- components/TitleScreen.tsx
+++ components/TitleScreen.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import React from 'react';
 import Image from 'next/image';
 
 type Props = {
@@ -8,24 +9,25 @@ type Props = {
 
 export default function TitleScreen({ onStart }: Props) {
   return (
-    <div className="relative w-screen h-screen bg-white overflow-hidden">
-      {/* 背景画像 */}
+    <div className="relative w-screen h-screen bg-black overflow-hidden">
+      {/* タイトル背景画像 */}
       <Image
         src="/images/title_main_with_start.png"
         alt="タイトル画面"
-        fill
-        className="object-cover pointer-events-none"
+        layout="fill"
+        objectFit="cover"
+        className="pointer-events-none"
         priority
       />
 
-      {/* はじめるボタン */}
+      {/* ▶ はじめるボタン */}
       <button
         onClick={onStart}
         className="absolute z-50 px-6 py-3 bg-blue-600 text-white rounded shadow text-base font-semibold"
         style={{
           bottom: '5%',
           left: '50%',
-          transform: 'translateX(calc(-50% + 4vw))',
+          transform: 'translateX(calc(-50% + 4vw))', // ← 中央より少し左に寄せる
         }}
       >
         ▶ はじめる
