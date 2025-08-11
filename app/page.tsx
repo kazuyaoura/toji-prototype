0560c341804020c1542f4e7bc795ade22ab4a7fe
kazuyaoura <otu_nishiguchi13@yahoo.co.jp>
2025-08-11 18:06:09 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,13 @@
+// app/page.tsx
 'use client';
 
 import GameContainer from '@/components/GameContainer';
-import MoneyDisplay from '@/components/MoneyDisplay';
 import { MoneyProvider } from '@/contexts/MoneyContext';
 
 export default function Home() {
   return (
     <MoneyProvider>
-      <>
-        <MoneyDisplay />
-        <GameContainer />
-      </>
+      <GameContainer />
     </MoneyProvider>
   );
 }
