ea03d8f124e0979094c18ebc7e4658558348a076
kazuyaoura <otu_nishiguchi13@yahoo.co.jp>
2025-08-11 17:39:51 +0900

    Update TitleScreen.tsx

--- components/TitleScreen.tsx
+++ components/TitleScreen.tsx
@@ -1,37 +1,33 @@
-// components/NoticeScreen.tsx
 'use client';
 
 import Image from 'next/image';
 
 type Props = {
-  onNext: () => void;
+  onStart: () => void;
 };
 
-export default function NoticeScreen({ onNext }: Props) {
+export default function TitleScreen({ onStart }: Props) {
   return (
     <div className="relative w-screen h-screen bg-white overflow-hidden">
-      {/* 背景画像（タイトルと同じ方針） */}
       <Image
-        src="/images/ui_notice_game_disclaimer.png"
-        alt="ご注意"
+        src="/images/title_main_with_start.png"
+        alt="タイトル画面"
         fill
+        className="object-cover pointer-events-none"
         priority
-        className="object-contain pointer-events-none"
       />
-
-      {/* ▶ 次へボタン（タイトル画面と同じ配置・サイズ感） */}
       <div
         style={{
           position: 'absolute',
           bottom: '5%',
           left: '50%',
-          transform: 'translateX(calc(-50% - 1vw))', // タイトルと同じく少し左に
+          transform: 'translateX(calc(-50% - 1vw))',
           width: 'min(75vw, 520px)',
           zIndex: 10,
         }}
       >
         <button
-          onClick={onNext}
+          onClick={onStart}
           style={{
             width: '100%',
             height: '48px',
@@ -43,7 +39,7 @@ export default function NoticeScreen({ onNext }: Props) {
             boxShadow: '0 2px 6px rgba(0,0,0,0.3)',
           }}
         >
-          ▶ 次へ
+          ▶ はじめる
         </button>
       </div>
     </div>
