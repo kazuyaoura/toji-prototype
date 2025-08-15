e22cc37bcd868a160fbe51072c20deac14814372
kazuyaoura <otu_nishiguchi13@yahoo.co.jp>
2025-08-15 17:23:46 +0900

    Update page.tsx

--- app/prologue/opening/page.tsx
+++ app/prologue/opening/page.tsx
@@ -2,14 +2,19 @@
 'use client';
 
 import NarrationScene from '@/components/NarrationScene';
+import { useCharacter } from '@/contexts/CharacterContext';
+import { protagonistLabel } from '@/lib/narrationHelpers';
 
 export default function PrologueOpening() {
+  const { character } = useCharacter();
+  const name = protagonistLabel(character);
+
   return (
     <NarrationScene
       sceneImg="/images/bg_narration_kichizaemon_intro.png"
       narratorImg="/images/kichizaemon_character.png"
       steps={[
-        '隆介よ、今年の冬はひとつ、お前に本蔵を任せたいと思うてな。',
+        `${name}よ、今年の冬はひとつ、お前に本蔵を任せたいと思うてな。`,
         '今津の杜氏が倒れてしもうてな……。本蔵のことはお前に託す。',
         '鈴も補佐につけておる。しっかり支えてくれるじゃろう。',
       ]}
