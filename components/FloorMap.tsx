b47c463118dfa3d44bde905ae03fcca1a64d8ba0
kazuyaoura <otu_nishiguchi13@yahoo.co.jp>
2026-05-06 17:39:35 +0900

    Update FloorMap.tsx

--- components/FloorMap.tsx
+++ components/FloorMap.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import Image from 'next/image';
-import DialogueBox from './DialogueBox';
 
 type Props = {
   onNext: () => void;
@@ -12,24 +11,35 @@ export default function FloorMap({ onNext }: Props) {
 工程ごとに場所も人もちゃうから、しっかり覚えておいてな！`;
 
   return (
-    <div className="w-screen h-screen bg-black flex flex-col overflow-hidden">
-      {/* 上：フロアマップ */}
-      <div className="relative flex-1">
+    <div
+      onClick={onNext}
+      className="fixed inset-0 bg-black flex flex-col overflow-hidden cursor-pointer"
+    >
+      {/* 上：背景 */}
+      <div className="relative flex-1 min-h-0">
         <Image
           src="/backgrounds/bg_floor_map.png"
           alt="フロアマップ"
           fill
-          className="object-cover pointer-events-none"
           priority
+          className="object-contain pointer-events-none bg-black"
         />
       </div>
 
-      {/* 下：共通DialogueBox */}
-      <DialogueBox
-        speaker="頭"
-        text={message}
-        onNext={onNext}
-      />
+      {/* 下：UI */}
+      <div className="bg-[#1b1b1b] border-t-4 border-white px-4 py-4 shrink-0">
+
+        {/* テキスト */}
+        <div className="text-white text-center text-sm leading-relaxed whitespace-pre-line mb-3">
+          {message}
+        </div>
+
+        {/* ガイド */}
+        <div className="text-center text-blue-300 text-xs animate-pulse">
+          ▼ TAP TO NEXT
+        </div>
+
+      </div>
     </div>
   );
 }
